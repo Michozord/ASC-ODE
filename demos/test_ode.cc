90b9dad711deade9f7b2629a202a60bb2c6e9eb6
Tobias Steinkellner <steinkellner.tobias1@gmail.com>
2024-01-07 20:37:16 +0100

    added electrical circuit example

--- demos/test_ode.cc
+++ demos/test_ode.cc
@@ -29,28 +29,60 @@ class MassSpring : public NonlinearFunction
   }
 };
 
+class Circuit : public NonlinearFunction{
+
+  double resistance = 1;
+  double capacity = 1;
+
+  size_t DimX() const override {return 2;}
+  size_t DimF() const override {return 2;}
+
+  void Evaluate(VectorView<double> x, VectorView<double> f) const override 
+  {
+    //x(0) - time //x(1) - y(t)
+    f(0)=1;   
+    f(1)=( std::cos(100* M_PI* x(0))-x(1) ) / (resistance*capacity);
+  }
+
+  void EvaluateDeriv (VectorView<double> x, MatrixView<double, ColMajor> df) const override
+  {
+    df = 0.0;
+    df(1,0) =(-100 * M_PI* std::sin(100 * M_PI* x(0))-x(1))/(resistance* capacity) ;
+    df(1,1) = -1 / (resistance*capacity);
+  }
+};
+
 
 int main()
 {
   double tend = 4*M_PI;
-  int steps = 100;
+  int steps = 1000;
   ASC_bla::Vector<double> y { 1, 0 };
   auto rhs = std::make_shared<MassSpring>();
   std::ofstream ost;
-  ost.open ("C:/Users/stein/Documents/ODE13.12/ASC-ODE/py_tests/output_ie.txt");
+  ost.open ("C:/Users/stein/Documents/ODE7.1.24/ASC-ODE/py_tests/output_ie.txt");
   SolveODE_IE(tend, steps, y, rhs,
               [&ost](double t, VectorView<double> y) { ost << t << "  " << y(0) << " " << y(1) << "\n"; });
   ost.close();
   y(0)=1; y(1)=0;
   std::ofstream ost2;
-  ost2.open ("C:/Users/stein/Documents/ODE13.12/ASC-ODE/py_tests/output_ee.txt");
+  ost2.open ("C:/Users/stein/Documents/ODE7.1.24/ASC-ODE/py_tests/output_ee.txt");
   SolveODE_EE(tend, steps, y, rhs,
               [&ost2](double t, VectorView<double> y) { ost2 << t << "  " << y(0) << " " << y(1) << "\n"; });
   ost2.close();
   y(0)=1; y(1)=0;
   std::ofstream ost3;
-  ost3.open ("C:/Users/stein/Documents/ODE13.12/ASC-ODE/py_tests/output_cn.txt");
+  ost3.open ("C:/Users/stein/Documents/ODE7.1.24/ASC-ODE/py_tests/output_cn.txt");
   SolveODE_CN(tend, steps, y, rhs,
               [&ost3](double t, VectorView<double> y) { ost3 << t << "  " << y(0) << " " << y(1) << "\n"; });
   ost3.close();
+
+  y(0)=0; y(1)=0;
+  tend=0.1;
+  auto rhs_circuit = std::make_shared<Circuit>();
+  std::ofstream ost4;
+  ost4.open ("C:/Users/stein/Documents/ODE7.1.24/ASC-ODE/py_tests/output_circuit.txt");
+  SolveODE_IE(tend, steps, y, rhs_circuit,
+              [&ost4](double t, VectorView<double> y) { ost4 << t << "  " << y(1) << "\n"; });
+  ost4.close();
 }
